122216f916e6408fe2b11e8b8b88d075930fc7bc
jhughs <jeffreyschmitt@gmail.com>
2014-10-06 13:33:23 -0500

    Cap Server vs. small server?

--- webrtc_signal_server.js
+++ webrtc_signal_server.js
@@ -30,7 +30,8 @@
 var http = require("http");
 var fs = require("fs");
 //var websocket = require("websocket").server;
-var websocket = require("ws").Server;  // JHS changed from "websocket" to "ws"
+var websocket = require("ws").server;  // JHS changed from "websocket" to "ws"
+										// JHS "Server" vs. "server" ????
 
 // general variables
 var port = process.env.PORT || 5000;  // port requirement for Heroku; was 1234;  jhs
